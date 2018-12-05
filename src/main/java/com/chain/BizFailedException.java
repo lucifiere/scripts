0a080da104d9c6b521616abe709090156882779c
XD.Wang <xiandui.wxd@alibabapictures.com>
2018-12-05 16:00:43 +0800

    补提交

--- src/main/java/com/chain/BizFailedException.java
+++ src/main/java/com/chain/BizFailedException.java
@@ -38,4 +38,28 @@ public class BizFailedException extends RuntimeException {
         return new BizFailedException(msg, code, null);
     }
 
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
 }
